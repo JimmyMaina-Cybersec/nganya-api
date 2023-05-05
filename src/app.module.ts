1c63ee2bb48576fe241e98d9ddff6e6ca0631368
Namianya <wekesa350@gmail.com>
2023-05-05 23:36:03 +0300

    global env

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,9 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     StationsModule,
     UsersModule,
