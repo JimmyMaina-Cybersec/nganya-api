c1a2cedbe70bc7e4fe85d8f09644c1c589761580
Namianya <wekesa350@gmail.com>
2023-05-06 15:27:41 +0300

    controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,23 +2,28 @@ import {
   Controller,
   Post,
   Body,
+  HttpCode,
+  HttpStatus,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/login.dto';
+import { Request } from 'express';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   login(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.signIn(createAuthDto);
   }
 
-  @Post()
-  signOut() {
-    return this.authService.signOut();
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  signOut(@CurrentUser('_id') userId: string) {
+    return this.authService.signOut(userId);
   }
 }
