319987779001caa383726fc8090b8dd0591e8e83
Namianya <wekesa350@gmail.com>
2023-05-07 08:23:27 +0300

    edit dto

--- src/vehicle-owners/dto/update-vehicle-owner.dto.ts
+++ src/vehicle-owners/dto/update-vehicle-owner.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVehicleOwnerDto } from './create-vehicle-owner.dto';
+import { IsString } from 'class-validator';
 
-export class UpdateVehicleOwnerDto extends PartialType(CreateVehicleOwnerDto) {}
+export class UpdateVehicleOwnerDto extends PartialType(
+  CreateVehicleOwnerDto,
+) {
+  @IsString()
+  _id: string;
+}
