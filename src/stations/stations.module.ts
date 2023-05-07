ee33870bbf081981a03cf4fc6d3e08252f5e9a4a
Namianya <wekesa350@gmail.com>
2023-05-07 05:45:50 +0300

    import schema

--- src/stations/stations.module.ts
+++ src/stations/stations.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { StationsService } from './stations.service';
 import { StationsController } from './stations.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Station,
+  StationSchema,
+} from './schema/station.schema';
 
 @Module({
   controllers: [StationsController],
-  providers: [StationsService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Station.name,
+        schema: StationSchema,
+      },
+    ]),
+  ],
+  providers: [StationsService],
 })
 export class StationsModule {}
