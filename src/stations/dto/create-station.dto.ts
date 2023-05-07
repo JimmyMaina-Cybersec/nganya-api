ac114c74bf86c67bcac2afc83ffcead45dd0a29c
Namianya <wekesa350@gmail.com>
2023-05-07 05:49:25 +0300

    create Dto

--- src/stations/dto/create-station.dto.ts
+++ src/stations/dto/create-station.dto.ts
@@ -1,1 +1,26 @@
-export class CreateStationDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class CreateStationDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  street: string;
+
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsArray()
+  destinations: string[];
+}
