8cfa94add18681f9e36e87fc35ec22ce54ec05db
Namianya <wekesa350@gmail.com>
2023-05-07 08:22:44 +0300

    applies schema

--- src/vehicle-owners/vehicle-owners.module.ts
+++ src/vehicle-owners/vehicle-owners.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { VehicleOwnersService } from './vehicle-owners.service';
 import { VehicleOwnersController } from './vehicle-owners.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  VehicleOwner,
+  VehicleOwnerSchema,
+} from './schema/vehicle-owner.schema';
 
 @Module({
   controllers: [VehicleOwnersController],
-  providers: [VehicleOwnersService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: VehicleOwner.name,
+        schema: VehicleOwnerSchema,
+      },
+    ]),
+  ],
+  providers: [VehicleOwnersService],
 })
 export class VehicleOwnersModule {}
