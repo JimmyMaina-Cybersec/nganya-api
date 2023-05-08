1c4c0eab049c4cc742a27628b36c12807e313a59
Namianya <wekesa350@gmail.com>
2023-05-08 05:06:19 +0300

    prep for ci cd

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3001);
+  await app.listen(80);
 }
 bootstrap();
