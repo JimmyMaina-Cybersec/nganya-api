e5e43f731665a91aee549c9e0c60478288687477
Namianya <wekesa350@gmail.com>
2023-05-08 16:44:08 +0300

    deleted

--- src/test-presence/test-presence.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestPresenceService } from './test-presence.service';
-import { TestPresenceGateway } from './test-presence.gateway';
-
-@Module({
-  providers: [TestPresenceGateway, TestPresenceService]
-})
-export class TestPresenceModule {}
