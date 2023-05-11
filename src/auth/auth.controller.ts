947ac37523b8f1548d01949169b6a0a26f5290dd
Namianya <wekesa350@gmail.com>
2023-05-12 00:10:58 +0300

    poorr status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,13 +16,11 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  @HttpCode(HttpStatus.OK)
   login(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.signIn(createAuthDto);
   }
 
   @Post('logout')
-  @HttpCode(HttpStatus.OK)
   signOut(@CurrentUser('_id') userId: string) {
     return this.authService.signOut(userId);
   }
