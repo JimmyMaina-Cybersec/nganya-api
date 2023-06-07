b74ebef53f80b2bee22a30e3f9f365c9b02fa22a
Namianya <wekesa350@gmail.com>
2023-06-07 04:24:05 +0300

    update availablility

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,35 +1,49 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { CreateTicketDto } from "./dto/create-ticket.dto";
-import { UpdateTicketDto } from "./dto/update-ticket.dto";
-import { JwtPayload } from "src/types/jwt-payload";
-import { InjectModel } from "@nestjs/mongoose";
-import { Ticket, TicketDocument } from "./schema/tickets.schema";
-import { Model } from "mongoose";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateTicketDto } from './dto/create-ticket.dto';
+import { UpdateTicketDto } from './dto/update-ticket.dto';
+import { JwtPayload } from 'src/types/jwt-payload';
+import { InjectModel } from '@nestjs/mongoose';
+import { Ticket, TicketDocument } from './schema/tickets.schema';
+import { Model } from 'mongoose';
+import { Availability, AvailabilityDocument } from 'src/schemas/Availability';
 
 @Injectable()
 export class TicketService {
   constructor(
     @InjectModel(Ticket.name)
-    private readonly ticketModel: Model<TicketDocument>
+    private readonly ticketModel: Model<TicketDocument>,
+    @InjectModel(Availability.name)
+    private readonly availabilityModel: Model<AvailabilityDocument>,
   ) {}
 
   async book(createTicketDto: CreateTicketDto, user: JwtPayload) {
     try {
-      if (user.role === "station agent" || user.role === "station manager") {
+      if (user.role === 'station agent' || user.role === 'station manager') {
         const ticket = await this.ticketModel.create({
           ...createTicketDto,
           station: user.station,
           sacco: user.sacco,
           addedBy: user._id,
         });
+        // update booked seatst on availability
+        const availability = await this.availabilityModel.findByIdAndUpdate(
+          createTicketDto.availability,
+          {
+            $addToSet: { bookedSeats: createTicketDto.bookedSeat },
+          },
+        );
+
         return ticket;
       }
       throw new HttpException(
-        "You are not allowed to book a ticket",
-        HttpStatus.FORBIDDEN
+        'You are not allowed to book a ticket',
+        HttpStatus.FORBIDDEN,
       );
     } catch (error) {
-      throw new HttpException(error.message, error.status);
+      throw new HttpException(
+        error.message || 'Server Error',
+        error.status || 500,
+      );
     }
   }
 
@@ -39,7 +53,7 @@ export class TicketService {
         .find({
           sacco: user.sacco,
         })
-        .select("-__v");
+        .select('-__v');
     } catch (error) {
       throw new HttpException(error.message, error.status);
     }
@@ -49,8 +63,8 @@ export class TicketService {
     try {
       await this.ticketModel
         .findById(id)
-        .populate("addedBy", "firstName secondName")
-        .select("-__v");
+        .populate('addedBy', 'firstName secondName')
+        .select('-__v');
     } catch (error) {
       throw new HttpException(error.message, error.status);
     }
