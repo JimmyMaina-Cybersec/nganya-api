bb08cd54636f63381744614d138961e5549db6b7
Jimmy-quiver <mainajimmy40@gmail.com>
2023-06-13 18:55:18 +0300

    Defined the entity file

--- src/presence/entities/presence.entity.ts
+++ src/presence/entities/presence.entity.ts
@@ -1,1 +1,24 @@
-export class Presence {}
+import {
+    Prop,
+    Schema,
+    SchemaFactory
+} from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type PresenceDocument = Presence & Document;
+@Schema({ timestamps: true })
+export class Presence extends Document {
+    @Prop({ required: true })
+    user: string;
+
+    @Prop({ default: false })
+    online: boolean;
+
+    @Prop({ default: null })
+    createdAt: Date;
+
+    @Prop({ default: null })
+    updatedAt: Date;
+}
+
+export const PresenceSchema = SchemaFactory.createForClass(Presence);
