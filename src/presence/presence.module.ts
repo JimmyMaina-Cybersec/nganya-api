e4daac2e099e6f66ff79496be34f199ce28d115c
Jimmy-quiver <mainajimmy40@gmail.com>
2023-06-13 18:53:02 +0300

    Added presenceSchema to imports & added providers

--- src/presence/presence.module.ts
+++ src/presence/presence.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
-import { PresenceService } from './presence.service';
+import { Module, forwardRef } from '@nestjs/common';
 import { PresenceGateway } from './presence.gateway';
+import { PresenceService } from './presence.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Presence, PresenceSchema } from './entities/presence.entity';
 
 @Module({
-  providers: [PresenceGateway, PresenceService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: Presence.name, schema: PresenceSchema },
+    ]),
+    forwardRef(() => PresenceModule)
+  ],
+  providers: [PresenceGateway, PresenceService],
 })
 export class PresenceModule {}
