47880a078b4c67c15cdba8dec046bac8a5fe09e3
Jimmy-quiver <mainajimmy40@gmail.com>
2023-06-20 14:08:05 +0300

    first commit

---
+++ src/sms/sms.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Body, Headers, HttpStatus, Post } from '@nestjs/common';
+import { SmsService } from "./sms.service";
+
+@Controller('sms')
+export class SmsController {
+    constructor(private readonly smsService: SmsService) {}
+
+    @Post()
+    async sendSms(
+        @Headers() headers: Record<string, string>,
+        @Body() body: { phone: string; message: string }): Promise<any> {
+        if ( !body.phone || !body.message ) {
+            return {
+                statusCode: HttpStatus.BAD_REQUEST,
+                message: 'Phone number and message are required',
+            };
+        }
+
+        try {
+            const result = await this.smsService.sendSMS(body.phone, body.message);
+            return {
+                statusCode: HttpStatus.OK,
+                message: 'Message sent successfully',
+                result,
+            };
+        } catch (error) {
+            return {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: 'An error occurred while sending the message',
+                error: error,
+            };
+        }
+    }
+}
