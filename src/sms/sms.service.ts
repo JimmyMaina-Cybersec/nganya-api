96f88a50bcb799091e90f16bea28023a28c65c99
Jimmy-quiver <mainajimmy40@gmail.com>
2023-06-20 14:08:25 +0300

    first commit

---
+++ src/sms/sms.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { AfricasTalkingOptions, SMS } from "africastalking";
+
+@Injectable()
+export class SmsService {
+    private africasTalking: SMS;
+
+    constructor() {
+        const options: AfricasTalkingOptions = {
+            apiKey: '9b41f958e5d5f80605aa61f0acc3f7150d382e37ebaa995eaa87623c9332dad9',
+            usrname: 'nganya'
+        };
+        this.africasTalking = new SMS(options);
+    }
+
+    async sendSMS(phone: string, message: string): Promise<any> {
+        try {
+            const result = await this.africasTalking.send({
+                to: phone,
+                message: message,
+            });
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
