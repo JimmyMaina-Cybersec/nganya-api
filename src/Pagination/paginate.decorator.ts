94ce872b6593a20c1475a95f7628d513364f1af0
Jimmy-quiver <mainajimmy40@gmail.com>
2023-07-10 14:03:33 +0300

    first commit

--- src/Pagination/paginate.decorator.ts
+++ src/Pagination/paginate.decorator.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from 'express';
+import { Model } from 'mongoose';
 
-export function Paginate(
-  collection: any,
-  filterConditions?: any,
-  responsePerPage: number = 20,
-  defaultPage: number = 1,
-  populateFields?: string[]
-) {
+type QueryFilter = { [field: string]: any };
+type PopulateFields = string[];
+
+export function Paginate() {
   return function (
     target: Record<string, any>,
     propertyKey: string,
@@ -18,21 +16,33 @@ export function Paginate(
       req: Request,
       res: Response
     ): Promise<any> {
-      const currentPage = Number(req.query.page) || defaultPage;
+      console.log(req.query);
+      const currentPage = Number(req.query.page) || 1;
+      const responsePerPage = Number(req.query.resPerPage) || 20;
       const skip = (currentPage - 1) * responsePerPage;
       const limit = responsePerPage;
 
       try {
-        let query = collection.find();
+        const model = getModelFromMetadata(target, propertyKey);
+
+        if (!model) {
+          return res.status(500).json({ error: 'Model not found' });
+        }
+
+        let query = model.find();
+
+        const paramTypes = Reflect.getMetadata('design:paramtypes', target, propertyKey);
+        const filterConditions = extractFilterConditions(req, paramTypes);
+        const populateFields = extractPopulateFields(req, paramTypes);
 
         if (filterConditions) {
           query = query.where(filterConditions);
         }
 
-        const totalCount = await query.countDocuments().exec();
+        const totalCount = await query.countDocuments();
         const totalPages = Math.ceil(totalCount / responsePerPage);
 
-        let paginatedQuery = originalMethod.call(this, skip, limit);
+        let paginatedQuery = originalMethod.call(this, req, query);
 
         if (populateFields && populateFields.length > 0) {
           for (const field of populateFields) {
@@ -40,7 +50,7 @@ export function Paginate(
           }
         }
 
-        const data = await paginatedQuery.exec();
+        const data = await paginatedQuery.skip(skip).limit(limit).exec();
 
         res.json({
           data,
@@ -55,4 +65,52 @@ export function Paginate(
 
     return descriptor;
   };
+}
+
+function getModelFromMetadata(target: Record<string, any>, propertyKey: string): Model<any> | undefined {
+  const constructor = target.constructor;
+
+  if (constructor && constructor.prototype) {
+    const model = constructor.prototype[propertyKey];
+
+    if (model instanceof Model) {
+      return model;
+    }
+  }
+
+  return undefined;
+}
+
+function extractFilterConditions(req: Request, paramTypes: any[]): QueryFilter | undefined {
+  const filterConditions: QueryFilter = {};
+
+  for (let i = 0; i < paramTypes.length; i++) {
+    const paramType = paramTypes[i];
+
+    if (paramType === Object && req.query) {
+      Object.entries(req.query).forEach(([key, value]) => {
+        filterConditions[key] = value;
+      });
+    }
+  }
+
+  return Object.keys(filterConditions).length > 0 ? filterConditions : undefined;
+}
+
+function extractPopulateFields(req: Request, paramTypes: any[]): PopulateFields | undefined {
+  const populateFields: PopulateFields = [];
+
+  for (let i = 0; i < paramTypes.length; i++) {
+    const paramType = paramTypes[i];
+
+    if (Array.isArray(paramType) && req.query && typeof req.query.populate === 'string') {
+      const fields = req.query.populate.split(',');
+
+      for (const field of fields) {
+        populateFields.push(field.trim());
+      }
+    }
+  }
+
+  return populateFields.length > 0 ? populateFields : undefined;
 }
