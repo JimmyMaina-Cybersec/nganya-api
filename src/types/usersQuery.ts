b6e689230302499fb08262e35b117370ea19a079
Jimmy-quiver <mainajimmy40@gmail.com>
2023-07-19 16:45:11 +0300

    role to allow array of string added _id to the interface

--- src/types/usersQuery.ts
+++ src/types/usersQuery.ts
@@ -3,5 +3,6 @@ import { ObjectId } from 'mongodb';
 export interface UsersQuery {
   station?: ObjectId | string;
   sacco?: ObjectId | string;
-  role?: string;
+  role?: string | string[];
+  _id?: ObjectId | string;
 }
