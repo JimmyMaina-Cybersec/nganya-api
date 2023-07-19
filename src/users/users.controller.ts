29c9b5729c27aaa855957aaa4b7a8adacfe11476
Jimmy-quiver <mainajimmy40@gmail.com>
2023-07-19 16:21:30 +0300

    Applied the pagination decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,8 @@ import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { JwtPayload } from 'src/types/jwt-payload';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { FindStationAgentsDto } from './dto/find-station-agents.dto';
+import { Pagination } from 'src/common/decorators/paginate.decorator';
+import PaginationQueryType from 'src/types/paginationQuery';
 
 @UseGuards(JwtGuard)
 @Controller('users')
@@ -31,8 +33,11 @@ export class UsersController {
   }
 
   @Get()
-  findAll(@CurrentUser() currentUser: JwtPayload, @Query() query: any) {
-    return this.usersService.findAllUsers(currentUser, query);
+  findAll(
+    @CurrentUser() currentUser: JwtPayload,
+    @Pagination() pagination: PaginationQueryType,
+  ) {
+    return this.usersService.findAllUsers(currentUser, pagination);
   }
 
   @Get('user/:id')
