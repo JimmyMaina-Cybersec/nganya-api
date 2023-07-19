3c08aff30d29606ca67e4fb65285eaf44e212f43
Jimmy-quiver <mainajimmy40@gmail.com>
2023-07-19 17:49:54 +0300

    enforces type safety for findAll

--- src/types/vehicleQuery.ts
+++ src/types/vehicleQuery.ts
@@ -3,4 +3,9 @@ import { ObjectId } from 'mongodb';
 export interface VehicleQuery {
   owner?: ObjectId | string;
   sacco?: ObjectId | string;
+  $or?: Array<
+    | { lastStation?: ObjectId | string }
+    | { currentStation?: ObjectId | string }
+    | { nextStation?: ObjectId | string }
+  >;
 }
