cd23f6427ac96a45ccb9ca28e5f6ae776894ac67
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-07 21:41:41 +0300

    added find all endpoint

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   UseGuards,
+  Query,
 } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
@@ -13,12 +14,33 @@ import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { JwtPayload } from 'src/types/jwt-payload';
+import { Pagination } from 'src/common/decorators/paginate.decorator';
+import PaginationQueryType from 'src/types/paginationQuery';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @UseGuards(JwtGuard)
+  @Get()
+  findAll(
+    @CurrentUser() user: JwtPayload,
+    @Query('transactionMethod') transactionMethod: string,
+    @Query('transactionType') transactionType: string,
+    @Query('agent') agent: string,
+    @Query('station') station: string,
+    @Pagination() pagination: PaginationQueryType,
+  ) {
+    return this.paymentsService.findAll(
+      user,
+      transactionMethod,
+      transactionType,
+      agent,
+      station,
+      pagination,
+    );
+  }
+
   @Post('lipa-na-mpesa')
   sendStk(
     @CurrentUser() user: JwtPayload,
