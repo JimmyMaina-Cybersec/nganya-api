9046e89215cf8879819f41b32191c356abe8f68f
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-07 12:05:38 +0300

    partial controller files implemented

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,34 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
-
-  @Post()
-  create(@Body() createPaymentDto: CreatePaymentDto) {
-    return this.paymentsService.create(createPaymentDto);
+  
+  @UseGuards(JwtGuard)
+  @Post('lipa-na-mpesa'){
+    
   }
-
-  @Get()
-  findAll() {
-    return this.paymentsService.findAll();
+  @Post('mpesa-till-number'){
+    
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.paymentsService.findOne(+id);
+  @Post('cash-payment'){
+    
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
-    return this.paymentsService.update(+id, updatePaymentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.paymentsService.remove(+id);
-  }
+  
 }
