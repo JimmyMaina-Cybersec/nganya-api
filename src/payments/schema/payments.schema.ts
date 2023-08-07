99ab8e9e9f7033a893fa2c61822290168a4a6f93
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-07 19:11:07 +0300

    modified payment schema

--- src/payments/schema/payments.schema.ts
+++ src/payments/schema/payments.schema.ts
@@ -1,12 +1,31 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, SchemaTypes, Types } from 'mongoose';
 
-export type LipaNaMpesaDocument = HydratedDocument<LipaNaMpesa>;
-export type MpesaTillNumberDocument = HydratedDocument<MpesaTillNumber>;
-export type CashPaymentDocument = HydratedDocument<CashPayment>;
+export type PaymentsDocument = HydratedDocument<Payments>;
 
 @Schema({ timestamps: true })
 export class Payments {
+  @Prop({ type: String, required: true })
+  TransactionMethod: string;
+
+  @Prop({ type: Number, required: true })
+  Amount: number;
+
+  @Prop({ default: null, type: String })
+  MpesaTransaction: string;
+
+  @Prop({ default: null, type: SchemaTypes.ObjectId, ref: 'Ticket' })
+  TicketId: Types.ObjectId;
+
+  @Prop({ default: null, type: SchemaTypes.ObjectId, ref: 'Customer' })
+  CustomerId: Types.ObjectId;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
+  ReceivingAgentId: Types.ObjectId;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'Station' })
+  AgentStation: Types.ObjectId;
+
   @Prop({ required: true, type: String })
   ServiceDescription: string;
 
@@ -14,64 +33,4 @@ export class Payments {
   status: string;
 }
 
-@Schema({ timestamps: true })
-export class LipaNaMpesa extends Payments {
-  @Prop({ required: true, type: String })
-  MerchantRequestID: string;
-
-  @Prop({ required: true, type: String })
-  CheckoutRequestID: string;
-
-  @Prop({ required: true, type: String })
-  ResultCode?: string;
-
-  @Prop({ required: true, type: String })
-  ResponseDescription: string;
-
-  @Prop({ required: true, type: String })
-  CustomerMessage: string;
-
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
-  UserId: Types.ObjectId;
-
-  @Prop({ required: true, type: String })
-  UserFirstName: string;
-
-  @Prop({ required: true, type: String })
-  UserSecondName: string;
-}
-
-@Schema({ timestamps: true })
-export class MpesaTillNumber extends Payments {
-  @Prop({ required: true, type: String })
-  MerchantRequestID: string;
-
-  @Prop({ required: true, type: String })
-  CheckoutRequestID: string;
-
-  @Prop({ required: true, type: String })
-  ResultCode?: string;
-
-  @Prop({ required: true, type: String })
-  ResponseDescription: string;
-
-  @Prop({ required: true, type: String })
-  CustomerMessage: string;
-
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
-  ReceivingAgentId: Types.ObjectId;
-
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'Station' })
-  AgentStation: Types.ObjectId;
-}
-
-@Schema({ timestamps: true })
-export class CashPayment extends Payments {
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
-  ReceivingAgentId: Types.ObjectId;
-}
-
-export const LipaNaMpesaSchema = SchemaFactory.createForClass(LipaNaMpesa);
-export const MpesaTillNumberSchema =
-  SchemaFactory.createForClass(MpesaTillNumber);
-export const CashPaymentschema = SchemaFactory.createForClass(CashPayment);
+export const PaymentsSchema = SchemaFactory.createForClass(Payments);
