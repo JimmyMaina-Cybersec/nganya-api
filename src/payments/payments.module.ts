470a27697171bda20a27942469c00bb5f110575f
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-07 19:09:24 +0300

    added schme as imports

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Payments, PaymentsSchema } from './schema/payments.schema';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: Payments.name, schema: PaymentsSchema },
+    ]),
+  ],
+  providers: [PaymentsService],
 })
 export class PaymentsModule {}
