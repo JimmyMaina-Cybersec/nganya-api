93bce01137a9c3da40649adb6896dc60460ad199
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-07 19:11:25 +0300

    created mpesaTransaction schema

---
+++ src/payments/schema/mpesa-transaction.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type MpesaTransactionsDocument = HydratedDocument<MpesaTransactions>;
+
+@Schema({ timestamps: true })
+export class MpesaTransactions {
+  @Prop({ required: true, type: String })
+  MerchantRequestID: string;
+
+  @Prop({ required: true, type: String })
+  CheckoutRequestID: string;
+
+  @Prop({ required: true, type: String })
+  ResponseCode: string;
+
+  @Prop({ required: true, type: String })
+  ResponseDescription: string;
+
+  @Prop({ required: true, type: String })
+  CustomerMessage: string;
+
+  @Prop({ default: null, type: Number })
+  Amount: number;
+
+  @Prop({ default: null, type: String })
+  MpesaReceiptNumber: string;
+
+  @Prop({ default: null, type: String })
+  TransactionDate: string;
+
+  @Prop({ default: null, type: String })
+  PhoneNumber: string;
+}
+export const MpesaTransactionSchema =
+  SchemaFactory.createForClass(MpesaTransactions);
