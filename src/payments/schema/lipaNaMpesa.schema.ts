1dda6ef8182ab5558ae2cbae579c6030f1866527
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-08 14:29:08 +0300

    added properties to schema

--- src/payments/schema/lipaNaMpesa.schema.ts
+++ src/payments/schema/lipaNaMpesa.schema.ts
@@ -1,8 +1,17 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, SchemaTypes, Types } from 'mongoose';
 
 export type LipaNaMpesaDocument = HydratedDocument<LipaNaMpesa>;
 
 @Schema({ timestamps: true })
-export class LipaNaMpesa {}
+export class LipaNaMpesa {
+  @Prop({ required: true, type: SchemaTypes.ObjectId, ref: 'Station' })
+  AgentStationId: Types.ObjectId;
+
+  @Prop({ default: null, type: SchemaTypes.ObjectId, ref: 'User' })
+  ReceivingAgentId: Types.ObjectId;
+
+  @Prop({ default: null, type: SchemaTypes.ObjectId, ref: 'User' })
+  SaaccoId: Types.ObjectId;
+}
 export const LipaNaMpesaDocument = SchemaFactory.createForClass(LipaNaMpesa);
