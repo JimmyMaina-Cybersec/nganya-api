7ca35c5c7b9259495697872219dea9e906712685
Paul Wekesa <wekesa350@gmail.com>
2023-08-11 19:11:23 +0300

    role type

---
+++ src/types/RoleIdType.ts
@@ -0,0 +1,7 @@
+export enum RoleIdType {
+    ADMIN = "rol_kx1uokuO9ALBZZ1j",
+    GENERAL_ADMIN = "rol_b6DmOiIJtiWjB1nV",
+    DRIVER = "rol_LDf7Kmggd8oWhcX2",
+    STATION_MANAGER = "rol_vYmA3VSYWphpOILR",
+    SERVICE_AGENT = "rol_xIpWHrsUYWqUJ7ul",
+}
