bd94c41ccc6fc20f7d48c2cde647314c1be898b6
Tomlee-abila <tomleeabila@gmail.com>
2023-08-13 20:18:57 +0300

    ticket permissions

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -7,53 +7,60 @@ import {
   Param,
   Delete,
   UseGuards,
+  SetMetadata,
 } from '@nestjs/common';
 import { TicketService } from './tickets.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
-import { OldJwtPayload } from 'src/types/jwt-payload';
-import { AuthGuard } from '@nestjs/passport';
-import { OldCurrentUser } from 'src/common/decorators/current-user.decorator';
+import { JwtPayload } from 'src/types/jwt-payload';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import PaginationQueryType from '../types/paginationQuery';
 import { Pagination } from '../common/decorators/paginate.decorator';
+import { AuthorizationGuard } from 'src/auth/guards/authorization-guard.service';
+import { PermissionsGuard } from 'src/auth/guards/permissions/permissions.guard';
+import { UserPermissions } from 'src/types/PermissionType';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthorizationGuard, PermissionsGuard)
 @Controller('tickets')
 export class TicketController {
-  constructor(private readonly ticketService: TicketService) { }
+  constructor(private readonly ticketService: TicketService) {}
 
+  @SetMetadata('permissions', [UserPermissions.BOOK_TICKET])
   @Post('book-ticket')
   create(
     @Body() createTicketDto: CreateTicketDto,
-    @OldCurrentUser() user: OldJwtPayload,
+    @CurrentUser() user: JwtPayload,
   ) {
     return this.ticketService.book(createTicketDto, user);
   }
 
+  @SetMetadata('permissions', [UserPermissions.READ_SERVICE_AGENT_REPORTS])
   @Get()
   findAll(
-    @OldCurrentUser() user: OldJwtPayload,
+    @CurrentUser() user: JwtPayload,
     @Pagination() pagination: PaginationQueryType,
   ) {
     return this.ticketService.findAll(user, pagination);
   }
 
+  @SetMetadata('permissions', [UserPermissions.READ_TICKETS])
   @Get('ticket/:id')
   findOne(@Param('id') id: string) {
     return this.ticketService.findOne(id);
   }
 
+  @SetMetadata('permissions', [UserPermissions.UPDATE_TICKETS])
   @Patch('update-ticket/:id')
   update(
     @Param('id') id: string,
     @Body() updateTicketDto: UpdateTicketDto,
-    @OldCurrentUser() user: OldJwtPayload,
+    @CurrentUser() user: JwtPayload,
   ) {
     return this.ticketService.update(id, updateTicketDto, user);
   }
 
   @Delete('delete-ticket/:id')
-  remove(@Param('id') id: string, @OldCurrentUser() user: OldJwtPayload) {
+  remove(@Param('id') id: string, @CurrentUser() user: JwtPayload) {
     return this.ticketService.remove(id, user);
   }
 }
