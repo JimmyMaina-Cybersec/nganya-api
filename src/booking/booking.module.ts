385e6f8a7d5bba95ba8da5276653fc2356c1883c
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-17 16:38:50 +0300

    implemented booking's schema to be used in reports

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingGateway } from './booking.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookingSchema } from './schema/booking.schema';
 
 @Module({
   providers: [BookingGateway, BookingService],
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Booking', schema: BookingSchema }]),
+  ],
 })
 export class BookingModule {}
--- src/booking/schema/booking.schema.ts
+++ src/booking/schema/booking.schema.ts
@@ -1,1 +1,6 @@
+import { SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BookingDocument = HydratedDocument<Booking>;
 export class Booking {}
+export const BookingSchema = SchemaFactory.createForClass(Booking);
