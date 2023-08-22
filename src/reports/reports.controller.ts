6762dacc968b2418466621e6a2baffaac9249ddf
Jimmy-quiver <mainajimmy40@gmail.com>
2023-08-22 08:44:10 +0300

    removed day as param

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -14,17 +14,17 @@ export class ReportsController {
   // agent (READ_AGENT_''_REPORT)
   // -parcels, bookings and money collections for the agent
   @SetMetadata('permissions', UserPermissions.READ_AGENT_PARCELS_REPORT)
-  @Get('/agent-report/parcels/:day')
+  @Get('/agent-report/parcels')
   findAgentParcelReport(@CurrentUser() agent: JwtPayload) {
     return this.reportsService.findAgentParcelReport(agent);
   }
   @SetMetadata('permissions', UserPermissions.READ_AGENT_BOOKINGS_REPORT)
-  @Get('/agent-report/bookings/:day')
+  @Get('/agent-report/bookings')
   findAgentBookingsReport(@CurrentUser() agent: JwtPayload) {
     return this.reportsService.findAgentBookingsReport(agent);
   }
   @SetMetadata('permissions', UserPermissions.READ_AGENT_COLLECTIONS_REPORT)
-  @Get('/agent-report/money-collections/:day')
+  @Get('/agent-report/money-collections')
   findAgentCollectionsReport(@CurrentUser() agent: JwtPayload) {
     return this.reportsService.findAgentCollectionsReport(agent);
   }
@@ -32,17 +32,17 @@ export class ReportsController {
   // manager(READ_AGENTS_''_REPORTS)
   // -parcels, bookings and money collections for the station
   @SetMetadata('permissions', UserPermissions.READ_AGENTS_PARCELS_REPORTS)
-  @Get('/manager-report/parcels/:day')
+  @Get('/manager-report/parcels')
   findManagerParcelReport(@CurrentUser() manager: JwtPayload) {
     return this.reportsService.findManagerParcelReport(manager);
   }
   @SetMetadata('permissions', UserPermissions.READ_AGENTS_BOOKINGS_REPORTS)
-  @Get('/manager-report/bookings/:day')
+  @Get('/manager-report/bookings')
   findManagerBookingsReport(@CurrentUser() manager: JwtPayload) {
     return this.reportsService.findManagerBookingsReport(manager);
   }
   @SetMetadata('permissions', UserPermissions.READ_AGENTS_COLLECTIONS_REPORTS)
-  @Get('/manager-report/money-collections/:day')
+  @Get('/manager-report/money-collections')
   findManagerCollectionsReport(@CurrentUser() manager: JwtPayload) {
     return this.reportsService.findManagerCollectionsReport(manager);
   }
@@ -50,17 +50,17 @@ export class ReportsController {
   // administrator and gen admin (READ_STATIONS_''_REPORTS)
   // -parcels, bookings and money collections for any station
   @SetMetadata('permissions', UserPermissions.READ_STATIONS_PARCELS_REPORTS)
-  @Get('/station/parcels/:stationId/:day')
+  @Get('/station/parcels/:stationId')
   findStationParcelsReports(@Param(':stationId') stationId: string) {
     return this.reportsService.findStationParcelsReports(stationId);
   }
   @SetMetadata('permissions', UserPermissions.READ_STATIONS_BOOKINGS_REPORTS)
-  @Get('/station/bookings/:stationId/:day')
+  @Get('/station/bookings/:stationId')
   findStationBookingsReports(@Param(':stationId') stationId: string) {
     return this.reportsService.findStationBookingsReports(stationId);
   }
   @SetMetadata('permissions', UserPermissions.READ_STATIONS_COLLECTIONS_REPORTS)
-  @Get('/station/money-collections/:stationId/:day')
+  @Get('/station/money-collections/:stationId')
   findStationCollectionsReports(@Param(':stationId') stationId: string) {
     return this.reportsService.findStationCollectionsReports(stationId);
   }
